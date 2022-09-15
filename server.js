a0abb26b1090b2f11d11c48a47b5823442a2d463
Pthakur30 <thakurpratham3011@gmail.com>
2022-09-15 17:18:04 -0400

    hello

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const exp = require("express");
 const app = exp();
+const path = require("path");
 
 app.get("/", function (req, res) {
-  res.send("Hello");
+  //res.send("Hello");
+  console.log(__dirname);
+
+  res.sendFile(path.join(__dirname, "home.html"));
 });
 
 var port = process.env.PORT || 3000;
